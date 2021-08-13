c94c368c5b43641ef147d330c23fd5d75e3d373f
SilverBoom <dduynam.vt@gmail.com>
2021-08-14 01:58:52 +0700

    Add OOP property to example

--- C++/study/concept/SOLID/pizza_store/pizzastore.cpp
+++ C++/study/concept/SOLID/pizza_store/pizzastore.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+// #abstract class -- binding within one certain scope.
 class Pizza
 {
 protected:
@@ -9,11 +10,13 @@ protected:
     std::vector<std::string> topping;
 
 public:
-    Pizza(std::string _name, std::string _dough, std::string _sauce, std::vector<std::string> _topping)
+    Pizza(std::string _name = "", std::string _dough = "", std::string _sauce = "", std::vector<std::string> _topping = {""})
         : name(_name),
           dough(_dough),
           sauce(_sauce),
           topping(_topping) {}
+
+    virtual void makeAnAbstractionObject() = 0; // make this class become abstraction class.
 
     void Prepare()
     {
@@ -37,28 +40,115 @@ public:
         std::cout << "Cutting " << name << std::endl;
     }
 
-    void Box()
+    virtual void Box()
     {
         std::cout << "Boxing " << name << std::endl;
     }
 };
 
+class CheesePizza : public Pizza
+{
+public:
+    CheesePizza()
+    {
+        name = "The best cheese pizza";
+        dough = "Very thin dough";
+        sauce = "Very spicy sauce";
+        topping.push_back("Black olives");
+        topping.push_back("More Cheese");
+    }
+
+    void makeAnAbstractionObject() {}
+};
+
+class GreekPizza : public Pizza
+{
+public:
+    GreekPizza()
+    {
+        name = "The moderate pizza";
+        dough = "Thin dough";
+        sauce = "Chilli sauce";
+        topping.push_back("Tomato");
+        topping.push_back("Seafood");
+    }
+    void makeAnAbstractionObject() {}
+    void Box() override{
+        std::cout << "Boxing...And luckily You will get one pizza addtionally since we are in promotional period" << std::endl;
+    }
+};
+
+class PepperoniPizza : public Pizza
+{
+public:
+    PepperoniPizza()
+    {
+        name = "The best pepperoni pizza";
+        dough = "Thick dough";
+        sauce = "Sweet sauce";
+        topping.push_back("Tomato");
+    }
+    void makeAnAbstractionObject() {}
+};
+
+enum Type
+{
+    CHEESE,
+    GREEK,
+    PEPPERONI,
+    DEFAULT
+};
+
+Type convertType(const std::string &type)
+{
+    if (type == "cheese")
+        return Type::CHEESE;
+    else if (type == "greek")
+        return Type::GREEK;
+    else if (type == "pepperoni")
+        return Type::PEPPERONI;
+    else
+        return Type::DEFAULT;
+}
+
 class PizzaStore
 {
 public:
-    void orderPizza()
+    void orderPizza(std::string type)
     {
-        Pizza *pizza = new Pizza("Pizza 1", "Thin dough", "Chilli sauce", {"Black Olives", "Onions"});
-        pizza->Prepare();
-        pizza->Bake();
-        pizza->Cut();
-        pizza->Box();
+        Pizza *pizza;
+
+        switch (convertType(type))
+        {
+        case CHEESE:
+            pizza = new CheesePizza();
+            break;
+        case GREEK:
+            pizza = new GreekPizza();
+            break;
+        case PEPPERONI:
+            pizza = new PepperoniPizza();
+            break;
+        default:
+            std::cout << "You have not chosen the pizza yet!" << std::endl;
+            break;
+        }
+
+        if (!pizza){
+            pizza->Prepare();
+            pizza->Bake();
+            pizza->Cut();
+            pizza->Box();
+        }
     }
 };
 
 int main()
 {
     PizzaStore *store = new PizzaStore();
-    store->orderPizza();
+    std::cout << "Choose a pizza that you want: ";
+    std::string yourChoice;
+    std::cin >> yourChoice;
+    store->orderPizza(yourChoice);
     return 0;
 }
